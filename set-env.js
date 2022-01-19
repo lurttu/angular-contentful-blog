fae71b12e91611602072c0003875136d37097756
Arttu Matilainen <arttu.matilainen@solteq.com>
2022-01-19 17:12:45 +0200

    bump deps

--- set-env.js
+++ set-env.js
@@ -1,28 +1,30 @@
-const fs = require('fs');
+const fs = require("fs");
 // Configure Angular `environment.ts` file path
-const targetPath = './src/environments/environment.ts';
-// Load node modules
-const colors = require('colors');
-require('dotenv').config({path:'.env'});
+const targetPath = "./src/environments/environment.ts";
+require("dotenv").config({ path: ".env" });
 // `environment.ts` file structure
 const envConfigFile = `export const environment = {
     production: false,
     contentful: {
       space: "${process.env.SPACE_ID}",
       accessToken: "${process.env.CONTENTFUL_ACCESS_TOKEN}",
-      
+
       contentTypeIds: {
         blogPost: 'blogPost' // EXAMPLE
       }
     }
   };
 `;
-console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
-console.log(colors.grey(envConfigFile));
+console.log(
+  "The file `environment.ts` will be written with the following content: \n"
+);
+console.log(envConfigFile);
 fs.writeFile(targetPath, envConfigFile, function (err) {
-   if (err) {
-       throw console.error(err);
-   } else {
-       console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
-   }
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(
+      `Angular environment.ts file generated correctly at ${targetPath} \n`
+    );
+  }
 });
